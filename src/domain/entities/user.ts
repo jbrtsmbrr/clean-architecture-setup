14c2767aa696d3990c45ba733d265b79547e6a65
jbrtsmbrr <jbrt.dev@gmail.com>
2024-10-24 15:31:31 +0800

    init: sample entity

---
+++ src/domain/entities/user.ts
@@ -0,0 +1,28 @@
+class User {
+  private name: string;
+  private age: number;
+
+  private constructor(name: string, age: number) {
+    this.age = age;
+    this.name = name
+  }
+
+  public static create({ name, age }: { name: string, age: number }) {
+
+    return new User(name, age);
+  }
+
+  public validate() {
+    // Let say we don't allow user under 18
+    if (this.age < 18)
+      return false;
+
+    if (this.name.length <= 1)
+      return false;
+
+    return true;
+  }
+}
+
+
+export default User;
