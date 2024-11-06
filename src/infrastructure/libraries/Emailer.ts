ad8781f462403f3ee0abcdc11c181a8876fa3337
jbrtsmbrr <jbrt.dev@gmail.com>
2024-11-06 15:38:29 +0800

    init: sample emailer implementation

---
+++ src/infrastructure/libraries/Emailer.ts
@@ -0,0 +1,9 @@
+import { IEmailer } from "../../adapters/interfaces/libraries/IEmailer"
+
+export class GoogleEmailer implements IEmailer {
+  send() {}
+}
+
+export class YahooEmailer implements IEmailer {
+  send() {}
+}
