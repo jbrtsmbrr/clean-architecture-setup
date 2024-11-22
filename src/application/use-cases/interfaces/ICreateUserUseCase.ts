6f94b91807408db139d6ac1cfa14b4884fe90e92
jbrtsmbrr <jbrt.dev@gmail.com>
2024-11-22 16:28:38 +0800

    changed: folder structure

--- src/application/use-cases/interfaces/ICreateUserUseCase.ts
+++
@@ -1,6 +0,0 @@
-import { UserOutput } from "../../../infrastructure/adapters/output-bound";
-import { CreateUserInput } from "../../ports/repositories/IUserRepository";
-
-export interface ICreateUserUseCase {
-  execute(input: CreateUserInput): Promise<UserOutput>;
-}
