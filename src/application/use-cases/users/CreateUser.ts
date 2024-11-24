911234d4f0c86fefe1597e7e9a922d8873ec0b4c
jbrtsmbrr <jbrt.dev@gmail.com>
2024-11-24 21:42:58 +0800

    updated: apply changes in user entity

--- src/application/use-cases/users/CreateUser.ts
+++ src/application/use-cases/users/CreateUser.ts
@@ -18,19 +18,25 @@ class CreateUserUseCase implements ICreateUserUseCase {
 
   public async execute(user_input: CreateUserInput): Promise<UserId> {
 
-    const user = User.create({ name: user_input.name, age: user_input.age })
+    const user = User.create({
+      name: user_input.name,
+      lastname: user_input.lastname,
+      password: user_input.password,
+      age: user_input.age,
+    });
+
     const is_valid = user.validate();
 
     // if (is_valid)
     //   return false;
 
     const inserted_id = await this.repository.create({
-      name: 'John',
-      lastname: 'Doe',
-      password: 'password',
-      age: 21
+      name: user.getName(),
+      lastname: user.getLastName(),
+      password: user.getLastName(),
+      age: user.getAge()
     })
-    
+
     this.emailer.send();
 
     return inserted_id;
