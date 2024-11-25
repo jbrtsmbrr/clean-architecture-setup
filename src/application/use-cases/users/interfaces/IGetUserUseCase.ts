1c53379c964521d0e4e156c14b77173481486d59
jbrtsmbrr <jbrt.dev@gmail.com>
2024-11-25 23:17:15 +0800

    changed: IGetUserUseCase now returning 'null' if data not exists

--- src/application/use-cases/users/interfaces/IGetUserUseCase.ts
+++ src/application/use-cases/users/interfaces/IGetUserUseCase.ts
@@ -2,5 +2,5 @@ import { UserId } from "./common";
 import { UserOutput } from "./output";
 
 export interface IGetUserUseCase {
-  execute(id: UserId): Promise<UserOutput>
+  execute(id: UserId): Promise<UserOutput | null>
 }
