2f9c7a6b7590847a95527baffab9194d06521822
jbrtsmbrr <jbrt.dev@gmail.com>
2024-11-27 00:31:12 +0800

    init: user repository mock

---
+++ tests/mocks/user_repository.mock.ts
@@ -0,0 +1,33 @@
+import { UserId } from "../../src/application/use-cases/users/interfaces/common";
+import { CreateUserInput } from "../../src/application/use-cases/users/interfaces/input";
+import { UserOutput } from "../../src/application/use-cases/users/interfaces/output";
+import IUserRepositoryPort from "../../src/interfaces/repositories/IUserRepository";
+
+
+export class MockUserRepository implements IUserRepositoryPort {
+  private current_id: number = 0;
+  private readonly users: Map<UserId, UserOutput> = new Map();
+
+  create(user: CreateUserInput): Promise<UserId> {
+    const inserted_id = (this.current_id + 1).toString();
+
+    this.users.set(inserted_id, {
+      id: inserted_id,
+      age: user.age,
+      name: user.name,
+      lastname: user.lastname
+    })
+
+    this.current_id += 1;
+
+    return Promise.resolve(inserted_id)
+  }
+
+  retrieve(id: UserId): Promise<UserOutput | null> {
+    const retrieved_user = this.users.get(id);
+
+    if (!retrieved_user) return Promise.resolve(null);
+
+    return Promise.resolve(retrieved_user);
+  }
+}
